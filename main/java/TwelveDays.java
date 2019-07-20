1ed8410f620b6b4e3f86c93e79669ba1de0bfbef
ShajithaMohammed <shajithaparveen28@gmail.com>
2019-07-20 13:00:38 +0530

    Approach-2 with static field.

--- main/java/TwelveDays.java
+++ main/java/TwelveDays.java
@@ -1,29 +1,22 @@
 class TwelveDays {
 
-    String[] days;
-    String[] gifts;
-
-    TwelveDays() {
-        days = new String[]{"first", "second", "third", "fourth", "fifth", "sixth", "seventh",
-                "eighth", "ninth", "tenth", "eleventh", "twelfth"};
-
-        gifts = new String[]{"a Partridge in a Pear Tree", "two Turtle Doves", "three French Hens",
-                "four Calling Birds", "five Gold Rings", "six Geese-a-Laying", "seven Swans-a-Swimming",
-                "eight Maids-a-Milking", "nine Ladies Dancing", "ten Lords-a-Leaping", "eleven Pipers Piping",
-                "twelve Drummers Drumming"};
-    }
+    private static String[] lyrics = {
+            "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.",
+            "On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.",
+            "On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+            "On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+            "On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+            "On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+            "On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+            "On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+            "On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+            "On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+            "On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+            "On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+    };
 
     String verse(int verseNumber) {
-
-        int verseNo = verseNumber - 1;
-        String lyrics = "On the " + days[verseNo] + " day of Christmas my true love gave to me: ";
-        for (int i = verseNo; i >= 0; i--) {
-            lyrics += gifts[i];
-            lyrics += (i > 0) ? (i - 1 == 0 ? ", and " : ", ") : ".";
-
-        }
-        lyrics += "\n";
-        return lyrics;
+        return lyrics[verseNumber - 1] + "\n";
     }
 
     String verses(int startVerse, int endVerse) {
@@ -38,3 +31,4 @@ class TwelveDays {
         return verses(1, 12);
     }
 }
+
